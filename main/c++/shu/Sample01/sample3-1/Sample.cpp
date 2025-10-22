25670227951d5c54cf57fa1fcc42e9af94a41cca
shumai0919 <gamityhimiyyru@gmail.com>
2025-10-22 02:09:37 +0000

    002

---
+++ main/c++/shu/Sample01/sample3-1/Sample.cpp
@@ -0,0 +1,15 @@
+#include "Sample.h"
+#include <iostream>
+
+using namespace std;
+void Sample::func1(){
+    cout << "func1" << endl;
+    a = 1;
+    b = 1;
+    func2();//  func2ないから、func1を呼び出す
+}
+void Sample::func2(){
+    a = 2;
+    b = 2;
+    cout << "a=" << a << "," << "b=" << b << endl;
+}

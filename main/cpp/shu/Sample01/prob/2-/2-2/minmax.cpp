f6ab2ccbc00584924623513693589e0535d3bb9e
shumai0919 <gamityhimiyyru@gmail.com>
2025-10-27 23:40:30 +0000

    006

---
+++ main/cpp/shu/Sample01/prob/2-/2-2/minmax.cpp
@@ -0,0 +1,25 @@
+#include "minmax.h"
+
+int MinMax::max(int n1,int n2,int n3){
+    int max=n1;
+    if(max<n2){
+        max=n2
+    }
+    if(max<n3){
+        max=n3
+    }
+    return max;
+}
+
+int MinMax::min(int n1,int n2,int n3){
+    int min=n1;
+    if(min>n2){
+        min=n2
+    }
+    if(min>n3){
+        min=n3
+    }
+    return min;
+}
+
+
---
+++ main/cpp/shu/Sample01/prob/2-/2-2/minmax.h
@@ -0,0 +1,10 @@
+#ifndef _KEISAN_H_
+#define _KEISAN_H_
+
+class MinMax{
+    public:
+    int min(int n1,int n2,int n3);
+    int max(int n1,int n2,int n3);
+};
+
+#endif

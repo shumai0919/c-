d3858e39ec1672c9610f688899d1e732d32dd59e
shumai0919 <gamityhimiyyru@gmail.com>
2025-10-27 23:50:27 +0000

    007

---
+++ main/cpp/shu/Sample01/prob/2-/2-2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "minmax.h"
+
+using namespace std;
+
+int main(void){
+    MinMax m;
+    k.a=4;
+    k.b=3;
+    cout<<k.a<<"+"<<k.b<<"="<<k.add()<<endl;
+    cout<<k.a<<"-"<<k.b<<"="<<k.sub()<<endl;
+    return 0;
+}

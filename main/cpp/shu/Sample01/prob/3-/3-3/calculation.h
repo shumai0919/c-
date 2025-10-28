91d73045d3480a9572154283f13c10e075712255
shumai0919 <gamityhimiyyru@gmail.com>
2025-10-28 06:25:28 +0000

    013

--- main/cpp/shu/Sample01/prob/3-/3-3/calculation.h
+++ main/cpp/shu/Sample01/prob/3-/3-3/calculation.h
@@ -7,5 +7,15 @@ using namespace std;
 
 class Calculation{
     private:
-        
-}
+        int m_number1;
+        int m_number2;
+    public:
+        void setNumber1();
+        void setNumber2();
+        int getNumber1();
+        int getNumber2();
+        int add();
+        int sub();
+};
+
+#endif

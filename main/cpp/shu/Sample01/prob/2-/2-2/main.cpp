175583c1a88607bbef3bb2a3525c83479b050903
shumai0919 <gamityhimiyyru@gmail.com>
2025-10-28 00:50:28 +0000

    010

--- main/cpp/shu/Sample01/prob/2-/2-2/main.cpp
+++ main/cpp/shu/Sample01/prob/2-/2-2/main.cpp
@@ -8,7 +8,7 @@ int main(void){
     int a=4;
     int b=2;
     int c=7;
-    cout << a << "と" << b << "と" << c << "のうち、最大のものは" << m.max(a,b) << endl;
-    cout << a << "と" << b << "と" << c << "のうち、最小のものは" << m.min(a,b) << endl;
+    cout << a << "と" << b << "と" << c << "のうち、最大のものは" << m.max(a,b,c) << endl;
+    cout << a << "と" << b << "と" << c << "のうち、最小のものは" << m.min(a,b,c) << endl;
     return 0;
 }
--- main/cpp/shu/Sample01/prob/2-/2-2/minmax.cpp
+++ main/cpp/shu/Sample01/prob/2-/2-2/minmax.cpp
@@ -3,10 +3,10 @@
 int MinMax::max(int n1,int n2,int n3){
     int max=n1;
     if(max<n2){
-        max=n2
+        max=n2;
     }
     if(max<n3){
-        max=n3
+        max=n3;
     }
     return max;
 }
@@ -14,10 +14,10 @@ int MinMax::max(int n1,int n2,int n3){
 int MinMax::min(int n1,int n2,int n3){
     int min=n1;
     if(min>n2){
-        min=n2
+        min=n2;
     }
     if(min>n3){
-        min=n3
+        min=n3;
     }
     return min;
 }

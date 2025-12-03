af9858549eddf4911149a47ebfcd1b4950f93277
shumai0919 <gamityhimiyyru@gmail.com>
2025-12-03 10:32:38 +0000

    blacjack002

--- main/c/BlackJack.c
+++ main/c/BlackJack.c
@@ -5,13 +5,14 @@
 #include <unistd.h>
 #include <string.h>
 #define WAR_FUND 300000
+#define DEBUGMODE 0
 
-/*void showPossessionCards(int *possession){
+void showPossessionCards(int *possession){
     int length = sizeof(possession) / sizeof(int);
     for(int k=0;k<length;k++){
         printf("%d ",possession[k]);
     }
-}*/
+}
 
 char getCardMark(int card){
     int num_mark;
@@ -46,20 +47,36 @@ int getCardNum(int card){
 }
 
 int main(void){
-    int player_scoretotal = 0,dealer_scoretotal = 0,hit_count = 0,betamount = 0,return_amount = 0,player_money = WAR_FUND,player_cardhand,score_drawingcard,num_selection,ace_selection;
+    int player_scoretotal = 0,dealer_scoretotal = 0,hit_count = 0,betamount = 0,return_amount = 0,player_money = WAR_FUND,player_cardhand,score_drawingcard,num_selection,ace_selection,debugMode = DEBUGMODE;
     float return_amount_magnifi;
     char win_or_loss = 'n',str_selection = 'h',cardMark;
-    int player_owncard[12] = {0},dealer_owncard[12] = {0},cardstorage_shdc[52] = {0};
+    int player_owncard[12] = {-1},dealer_owncard[12] = {-1},cardstorage_shdc[52] = {-1};
     srand((unsigned)time(NULL));
 
     while(1){
+        int first_employCard[2];
+        int HS_employCard[52];
+        for(int k = 0;k < 2;k++){
+            first_employCard[k] = -1;
+        }
+        for(int k = 0;k < 52;k++){
+            HS_employCard[k] = -1;
+        }
+
+        //最初に2枚引くカード(0~51)を指定できる（DEBUGMODEが1のとき）
+        first_employCard[0] = 0;first_employCard[1] = 10;
+
+        //hit,standで引くカード
+        HS_employCard[0] = 0+26;HS_employCard[1] = 0+39;
+        HS_employCard[2] = 1;HS_employCard[3] = 1+13;
+        HS_employCard[4] = 1+26;HS_employCard[5] = 1+39;
 
         for(int i = 0;i < 52;i++){
             cardstorage_shdc[i] = i+1;
         }
-                player_scoretotal = dealer_scoretotal = 0;
+        player_scoretotal = dealer_scoretotal = 0;
         for(int i = 0;i < 12; i++){
-            player_owncard[i] = dealer_owncard[i] = 0;
+            player_owncard[i] = dealer_owncard[i] = -1;
         }
         str_selection = 'n';
         ace_selection = '0';
@@ -97,30 +114,49 @@ int main(void){
 
         printf("\nプレイヤー、ディーラーに2枚カードを配ります...\n");
         usleep(1000000);
-        int rand1 = 0,rand2 = 0;
+        int rand1 = 0,rand2 = 0,rand3 = 0;
         for(int i = 0;i < 2;i++){
             for(int j = 0;j < 2;j++){
                 do{
-                    rand1 = rand() % 14;
-                }while(cardstorage_shdc[rand1] == 0);
+                    int CanExecuteDebug = 0;
+                    for(int k = 0;k < 52;k++){
+                        if(first_employCard[k] != -1){
+                            CanExecuteDebug = 1;
+                        }
+                    }
+                    if(debugMode == 1 && i == 0 && CanExecuteDebug == 1){                        
+                        do{
+                            rand3 = rand() % 2;
+                            rand1 = first_employCard[rand3];
+                        }while(rand1 == -1);
+                        first_employCard[rand3] = -1;
+                    }else{
+                        rand1 = rand() % 52;
+                    }
+                }while(cardstorage_shdc[rand1] == -1);
                 if(i == 0){
                     player_owncard[j] = cardstorage_shdc[rand1];
                     if(getCardNum(cardstorage_shdc[rand1]) == 1 && player_scoretotal+10 <= 21){
                         do{
                             printf("Aceを引きました。(1:'1'としてカウント、2:'11'としてカウント) ");
                             if(j == 1){
-                                printf("1枚目: %d",getCardNum(player_owncard[0]));
+                                printf("(1枚目: %d)",getCardNum(player_owncard[0]));
                             }
                             printf(" > ");
                             scanf("%d",&ace_selection);
                         }while(ace_selection != 1 && ace_selection != 2);
-                        if(ace_selection == 1){player_scoretotal += 1;} else{player_scoretotal += 11;}
+                        if(ace_selection == 1)
+                            {player_scoretotal += 1;
+                            }else{player_scoretotal += 11;}
                     }else if(getCardNum(cardstorage_shdc[rand1]) >= 11 && getCardNum(cardstorage_shdc[rand1]) <= 13){
                         player_scoretotal += 10;
                     }else{
                         player_scoretotal += getCardNum(cardstorage_shdc[rand1]);
                     }
-                    cardstorage_shdc[rand1] = 0;
+                    cardstorage_shdc[rand1] = -1;
+                    if(player_scoretotal == 21 && (getCardNum(player_owncard[0]) == 1 && getCardNum(player_owncard[1]) >=10) || (getCardNum(player_owncard[0]) >=10 && getCardNum(player_owncard[1]) == 1)){
+                        win_or_loss = 'w';
+                    }
                 }else{
                     dealer_owncard[j] = cardstorage_shdc[rand1];
                     if(getCardNum(cardstorage_shdc[rand1]) == 1 && dealer_scoretotal+10 <= 21){
@@ -130,21 +166,38 @@ int main(void){
                     }else{
                         dealer_scoretotal += getCardNum(cardstorage_shdc[rand1]);
                     }
-                    cardstorage_shdc[rand1] = 0;
+                    cardstorage_shdc[rand1] = -1;
                 }
             
             }
         }
-        
-        printf("プレイヤーの手札: %d, %d (合計: %d)\n",getCardNum(player_owncard[0]),getCardNum(player_owncard[1]),player_scoretotal);
-        printf("ディーラーの手札: %d, ? (合計: ?)\n",getCardNum(dealer_owncard[0]));
-        printf("\n");
-        hit_count = 0;
-        printf("- プレイヤーのターン -\n");
+        if(win_or_loss == 'n'){
+            printf("プレイヤーの手札: %c%d, ",getCardMark(player_owncard[0]),getCardNum(player_owncard[0]));
+            printf("%c%d (合計: %d)\n ",getCardMark(player_owncard[1]),getCardNum(player_owncard[1]),player_scoretotal);
+            printf("ディーラーの手札: %c%d, ? (合計: ?)\n",getCardMark(player_owncard[0]),getCardNum(dealer_owncard[0]));
+            printf("\n");
+            hit_count = 0;
+            printf("- プレイヤーのターン -\n");
+        }
         while(str_selection != 's'){
+            if(win_or_loss == 'w'){break;}
             do{
-                rand1 = rand() % 52;
-            }while(cardstorage_shdc[rand1] == 0);
+                int CanExecuteDebug = 0;
+                for(int k = 0;k < 52;k++){
+                    if(HS_employCard[k] != -1){
+                        CanExecuteDebug = 1;
+                    }
+                }
+                if(DEBUGMODE == 1 && CanExecuteDebug == 1){
+                    do{
+                        rand3 = rand() % 52;
+                        rand1 = HS_employCard[rand3];
+                    }while(rand1 == -1);                
+                }else{
+                    rand1 = rand() % 52;
+                }
+            }while(cardstorage_shdc[rand1] == -1);
+            HS_employCard[rand3] = -1;
             do{
                 if(hit_count == 0){
                     printf("'Hit', 'Stand' or 'DoubleDown?' (h/s/d) > ");
@@ -166,11 +219,11 @@ int main(void){
                 }else{
                     player_scoretotal += getCardNum(cardstorage_shdc[rand1]);
                 }
-                cardstorage_shdc[rand1] = 0;
+                cardstorage_shdc[rand1] = -1;
                 hit_count++;
                 printf("手札: ");
                 for(int k=0;k<2+hit_count;k++){
-                    printf("%d,",getCardNum(player_owncard[k]));
+                    printf("%c%d,",getCardMark(player_owncard[k]),getCardNum(player_owncard[k]));
                 }
                 printf("(合計: %d)\n", player_scoretotal);
                 if(player_scoretotal > 21){
@@ -185,12 +238,12 @@ int main(void){
             }
         }
         hit_count = 0;
-        if(win_or_loss != 'l'){
+        if(win_or_loss == 'n'){
             printf("\n- ディーラーのターン -\n");
             usleep(1000000);
         }
         while(dealer_scoretotal < 17){
-            if(win_or_loss == 'l'){break;}
+            if(win_or_loss == 'l' || win_or_loss == 'w'){break;}
             do{
                 rand1 = rand() % 52;
             }while(cardstorage_shdc[rand1] == 0);
@@ -202,7 +255,7 @@ int main(void){
             }else{
                 dealer_scoretotal += getCardNum(cardstorage_shdc[rand1]);
             }
-            cardstorage_shdc[rand1] = 0;
+            cardstorage_shdc[rand1] = -1;
             hit_count++;
             if(dealer_scoretotal > 21){
                 printf("ディーラーがバーストしました 。\n");
@@ -213,9 +266,8 @@ int main(void){
         if(win_or_loss == 'n'){
             usleep(1000000);
             printf("ディーラーのターンが終了しました。\n\n");
-        }
+            usleep(1000000);
 
-        if(win_or_loss == 'n'){
             printf("点数を比較します...\n");
             printf("プレイヤーの合計: %d\n",player_scoretotal);
             printf("ディーラーの合計: %d\n",dealer_scoretotal);
@@ -229,8 +281,10 @@ int main(void){
         }
         printf("\n");
         if(win_or_loss == 'w'){
+            if(player_scoretotal == 21 && (getCardNum(player_owncard[0]) == 1 && getCardNum(player_owncard[1]) >=10) || (getCardNum(player_owncard[0]) >=10 && getCardNum(player_owncard[1]) == 1)){
+                printf("プレイヤーがブラックジャックになりました\n");
+            }
             printf("プレイヤーの勝ち\n");
-
         }else if(win_or_loss == 'l'){
             printf("プレイヤーの負け\n");
         }else if(win_or_loss == 'd'){
@@ -243,41 +297,44 @@ int main(void){
             case 'w':{
                 //通常勝利
                 return_amount_magnifi = 1.5;
-                int n1 = getCardNum(player_owncard[1]);
-                int n2 = getCardNum(player_owncard[2]);
-                int n3 = getCardNum(player_owncard[3]);
-                char s1 = getCardMark(player_owncard[1]);
-                char s2 = getCardMark(player_owncard[2]);
-                char s3 = getCardMark(player_owncard[3]);
+                int n1 = getCardNum(player_owncard[0]);
+                int n2 = getCardNum(player_owncard[1]);
+                int n3 = getCardNum(player_owncard[2]);
+                char s1 = getCardMark(player_owncard[0]);
+                char s2 = getCardMark(player_owncard[1]);
+                char s3 = getCardMark(player_owncard[2]);
                 if(player_scoretotal == 21){
-                    if(player_owncard[3] != 0){
+                    if(player_owncard[2] != -1){
                         if(n1 == n2 && n2 == n3 && n1 == 7){
                             //スリーセブン
                             return_amount_magnifi = 7;
+                            player_cardhand = 6;
                             break;
                         }
                     }
-                    if(player_owncard[3] == 0){
+                    if(player_owncard[3] == -1){
                         if(((n1 == 1 && n2 == 11) || (n1 == 11 && n2 == 1)) && s1 == 'S' && s2 == 'S'){
                             //表ブラックジャック
-                            return_amount_magnifi = 8;
+                            return_amount_magnifi = 15;
                             player_cardhand = 1;
+                            break;
                         }else if(((n1 == 1 && n2 == 11) || (n1 == 11 && n2 == 1))){
                             if(!(s1 == 'S' && s2 == 'S')){
                                 //裏ブラックジャック
                                 return_amount_magnifi = 4;
                                 player_cardhand = 2;
+                                break;
                             }
                         }else if((n1 == 1 && n2 >=10) || (n1 >=10 && n2 == 1)){
                             //ブラックジャック
                             return_amount_magnifi = 3;
                             player_cardhand = 3;
+                            break;
                         }
-                        break;
                     }
                 }
-                if(player_owncard[6] != 0){
-                    if(player_owncard[7] != 0){
+                if(player_owncard[5] != -1){
+                    if(player_owncard[6] != -1){
                         //セブンカード
                         return_amount_magnifi = 5;
                         player_cardhand = 4;
@@ -301,40 +358,44 @@ int main(void){
             }
         }
         return_amount = return_amount * return_amount_magnifi;
+        usleep(1000000);
 
         printf("プレイヤーの手札: ");
         for(int k=0;k<12;k++){
-            if(player_owncard[k] != 0){
+            if(player_owncard[k] != -1){
                 cardMark = getCardMark(player_owncard[k]);
                 printf("%c", cardMark);
                 printf("%d,",getCardNum(player_owncard[k]));
             }
         }
-        if(player_cardhand != 0){
+        printf("\n");
+        if(win_or_loss == 'w'){
             printf("あなたの役：");
-            if(player_cardhand == 1){printf("裏ブラックジャック(倍率:%.1f)", return_amount_magnifi);}
-            if(player_cardhand == 2){printf("ブラックジャック(倍率:%.1f)", return_amount_magnifi);}
+            if(player_cardhand == 0){printf("なし(倍率:%.1f)", return_amount_magnifi);}
+            if(player_cardhand == 1){printf("表ブラックジャック(倍率:%.1f)", return_amount_magnifi);}
+            if(player_cardhand == 2){printf("裏ブラックジャック(倍率:%.1f)", return_amount_magnifi);}
             if(player_cardhand == 3){printf("ブラックジャック(倍率:%.1f)", return_amount_magnifi);}
             if(player_cardhand == 4){printf("セブンカード(倍率:%.1f)", return_amount_magnifi);}
             if(player_cardhand == 5){printf("シックスカード(倍率:%.1f)", return_amount_magnifi);}
+            if(player_cardhand == 6){printf("スリーセブン(倍率:%.1f)", return_amount_magnifi);}
             printf("\n");
         }
-        printf("(合計: %d)\n",player_scoretotal);
-        printf("ディーラーの手札: ");
+        printf(" 合計: %d\n",player_scoretotal);
+        printf("\nディーラーの手札: ");
         for(int k=0;k<12;k++){
-            if(dealer_owncard[k] != 0){
+            if(dealer_owncard[k] != -1){
                 cardMark = getCardMark(dealer_owncard[k]);
                 printf("%c", cardMark);
                 printf("%d,",getCardNum(dealer_owncard[k]));
             }
         }
-        printf("(合計: %d)\n",dealer_scoretotal);
-
-        printf("あなたは$%dをベットしました。\n",betamount);
+        printf("\n");
+        printf(" 合計: %d\n",dealer_scoretotal);
+        printf("\nあなたは$%dをベットしました。\n",betamount);
         printf("倍率: %.1f\n",return_amount_magnifi);
         if(win_or_loss == 'w'){
             printf("配当額: + $%d\n",return_amount);
-            printf("所持金:  $%d\n",player_money + betamount + return_amount);
+            printf("所持金:  $%d\n",player_money + return_amount);
         }else if(win_or_loss == 'l'){
             printf("損失額: -$%d\n",betamount);
             printf("所持金:  $%d\n",player_money);
@@ -343,13 +404,15 @@ int main(void){
             printf("所持金:  $%d\n",player_money + return_amount);
         }
         player_money += return_amount;
+        usleep(2000000);
 
-
+        //showPossessionCards(player_owncard);
         //for(int k=0;k<52;k++){printf("%d,",cardstorage_shdc[k]);}
 
         printf("\nゲームを終了しますか？ (1:はい 0:いいえ) > ");
         scanf("%d",&num_selection);
         if(num_selection == 1){
+            printf("/n");
             exit(0);
         }
         printf("\n");
--- main/cpp/shumai0919/Emscripten/Sample/Sample01/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, Emscripten!" << std::endl;
-    return 0;
-}
